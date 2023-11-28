b149216e82f1d06aeccc1b10a7f63e7dc7f705c6
Felipe-dot <felipeluis610@gmail.com>
2023-11-28 10:36:20 -0300

    :sparkles: Add new props to pokemon class

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,19 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  number;
+  name;
+  type;
+  types = [];
+  photo;
+  species;
+  height;
+  weight;
+  abilities = [];
+  abilitie;
 }
+
+//  Name
+// Type               Number
+// species
+// height
+// weight
+// abilities
